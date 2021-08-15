fba8e05b5dcf9c8a48fc9741efc51314e019d277
mizuki.nakamura <zhongcunmeiyue580@gmail.com>
2021-08-15 12:40:21 +0000

    team.rb2

--- team.rb
+++ team.rb
@@ -23,6 +23,8 @@ Giants = Team.new("Giants", 67, 45, 8)
 Tigers = Team.new("Tigers", 60, 53, 7)
 Dragons = Team.new("Dragons", 60, 55, 5)
 BayStars = Team.new("BayStars", 56, 58, 6)
+Carp = Team.new("Carp", 52, 56, 12)
+Swallows = Team.new("Swallows", 41, 69, 10)
 
 puts "各チームの勝敗情報"
 
@@ -30,3 +32,5 @@ Giants.show_team_result()
 Tigers.show_team_result()
 Dragons.show_team_result()
 BayStars.show_team_result()
+Carp.show_team_result()
+Swallows.show_team_result()
